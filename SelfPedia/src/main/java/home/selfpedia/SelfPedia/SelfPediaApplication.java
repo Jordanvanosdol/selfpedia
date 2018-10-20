9aa24a2f52340ef664889f75350e676df8e682c0
jordanvanosdol <jordanvanosdol@gmail.com>
2018-10-20 00:42:59 -0500

    my first commit

---
+++ SelfPedia/src/main/java/home/selfpedia/SelfPedia/SelfPediaApplication.java
@@ -0,0 +1,12 @@
+package home.selfpedia.SelfPedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfPediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfPediaApplication.class, args);
+	}
+}
